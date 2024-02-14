94d9a05ee4ab35d73fbb99328c997ed2be6e3dc4
victoriaprisco <p.victoria@wustl.edu>
2024-02-13 18:12:39 -0600

    port over to new repo

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from "./Components/Login"
+import Signup from './Components/Signup';
+import PendingApproval from './Components/PendingApproval'; 
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Home from "./Components/Home"
+import Dashboard from './Components/Dashboard';
+import ClassDetails from './Components/ClassDetails';
+import React, { useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+
+
+const App = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setCurrentUser(user);
+    });
+    return () => unsubscribe(); // Clean up subscription
+  }, []);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/pending-approval" element={<PendingApproval />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/class/:classId" element={<ClassDetails />} />
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
