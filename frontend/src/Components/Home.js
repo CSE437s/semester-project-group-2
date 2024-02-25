4652fce41835d17bc01fa8423d01da40fa7d265d
sammya24 <89807548+sammya24@users.noreply.github.com>
2024-02-25 16:30:13 -0600

    setup tailwind correctly

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,7 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { auth } from '../firebase'; 
 import React, { useEffect, useState } from 'react';
-import './Home.css';
 
 const Home = () => {
     const navigate = useNavigate();
@@ -22,32 +21,32 @@ const Home = () => {
     }
 
     return (
-        <div class="home-container">
-            <h1>Welcome to Online Office Hours!</h1>
-            <p class="description">
+        <div className="home-container bg-gray-100 p-8">
+            <h1 className="text-3xl font-bold mb-4">Welcome to Online Office Hours!</h1>
+            <p className="text-lg text-gray-800 mb-6">
                 Our platform provides a seamless solution for organizing and attending office hours
                 with instructors and TAs. Say goodbye to scattered Zoom links and hello to a centralized
                 hub for academic support.
             </p>
-            <h2>Key Features</h2>
-            <ul>
+            <h2 className="text-2xl font-bold mb-2">Key Features</h2>
+            <ul className="list-disc pl-6 mb-6">
                 <li>Create customizable pages for instructors and TAs</li>
                 <li>Join video calls, text chats, or utilize whiteboard elements</li>
                 <li>Find the perfect instructor for your learning style</li>
                 <li>Enroll in classes and keep track of upcoming office hours sessions</li>
             </ul>
-            <h2>How It Works</h2>
-            <p class="description">
+            <h2 className="text-2xl font-bold mb-2">How It Works</h2>
+            <p className="text-lg text-gray-800 mb-6">
                 Students and instructors can sign up for an account and start creating or joining
                 office hours sessions. Instructors can customize their virtual classrooms, while
                 students can easily find and join sessions that match their needs.
             </p>
-            <h2>Get Started</h2>
-            <p class="description">
-                Ready to enhance your learning experience? <a href="/login" class="login-link">Log in</a> now or 
-                <Link to="/signup" className="login-link"> sign up</Link> to get started!
+            <h2 className="text-2xl font-bold mb-2">Get Started</h2>
+            <p className="text-lg text-gray-800 mb-6">
+                Ready to enhance your learning experience? 
+                <a href="/login" className="text-blue-600 hover:underline">Log in</a> now or 
+                <Link to="/signup" className="text-blue-600 hover:underline ml-1">sign up</Link> to get started!
             </p>
-
         </div>
     );
 };
