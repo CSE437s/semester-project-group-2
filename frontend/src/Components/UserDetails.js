d7f057da8b624383cc702e7bdff123e49cf062be
sammya24 <89807548+sammya24@users.noreply.github.com>
2024-02-26 16:34:57 -0600

    hid go to login button

--- frontend/src/Components/UserDetails.js
+++ frontend/src/Components/UserDetails.js
@@ -81,10 +81,8 @@ const UserDetails = () => {
 
     if (!user) {
         return (
-            <div className="container mx-auto text-center mt-10">
-                <h1>Please sign in to view this page.</h1>
-                <button onClick={() => navigate("/login")} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Go to login</button>
-            </div>
+                <button onClick={() => navigate("/login")} className="hidden bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Go to login</button>
+        
         );
     }
 
