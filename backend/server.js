8cfc792fd6651977701735f707daeee1377d36ef
victoriaprisco <p.victoria@wustl.edu>
2024-02-26 11:38:48 -0600

    fix backend

--- backend/server.js
+++ backend/server.js
@@ -80,4 +80,5 @@ app.post("/api/fileUpload", upload.single('myFile'), (req, res, next) => {
     res.sendStatus(200);
 });
 
-app.listen(3001, () => console.log("Listening on port 3001"));
+const port = process.env.PORT || 3001
+app.listen(port, () => console.log("Listening on port " + port));
