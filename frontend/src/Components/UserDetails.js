025d95ba3b62052eceb302cb5c34b40141b20312
victoriaprisco <p.victoria@wustl.edu>
2024-02-27 00:10:04 -0600

    remove MORE warnings

--- frontend/src/Components/UserDetails.js
+++ frontend/src/Components/UserDetails.js
@@ -2,26 +2,26 @@ import { useEffect, useState } from "react";
 import { auth, db } from "../firebase";
 import { doc, updateDoc, getDoc } from "firebase/firestore";
 import { sendPasswordResetEmail, updateProfile } from "firebase/auth";
-import axios from "axios";
+// import axios from "axios";
 import { useNavigate, Link } from 'react-router-dom';
 import LogoutButton from './LogoutButton';
 
-const cleanFileName = (fileName) => {
-    var newFileName = ""
-    for (var i = 0; i < fileName.length; i++) {
-        const char = fileName.charAt(i)
-        if (char !== ' ' && char !== ' ' && char !== "(" && char !== ")") {
-            newFileName += char
-        }
-    }
-    return newFileName
-}
+// const cleanFileName = (fileName) => {
+//     var newFileName = ""
+//     for (var i = 0; i < fileName.length; i++) {
+//         const char = fileName.charAt(i)
+//         if (char !== ' ' && char !== ' ' && char !== "(" && char !== ")") {
+//             newFileName += char
+//         }
+//     }
+//     return newFileName
+// }
 
 const UserDetails = () => {
-    const DEBUGGING = false
-    const base_url = "https://carefully-certain-swift.ngrok-free.app"
-    const debugging_url = "http://localhost:3001"
-    const api_url = DEBUGGING ? debugging_url : base_url
+    // const DEBUGGING = false
+    // const base_url = "https://carefully-certain-swift.ngrok-free.app"
+    // const debugging_url = "http://localhost:3001"
+    // const api_url = DEBUGGING ? debugging_url : base_url
 
     const navigate = useNavigate();
     const [user, setUser] = useState();
