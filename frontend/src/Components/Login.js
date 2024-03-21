c9799c244ca9fa1409b4e11c618b9bf9a68b62a8
victoriaprisco <p.victoria@wustl.edu>
2024-03-20 22:55:23 -0500

    remove warning

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import axios from "axios";
 import {getUser, getCurrentUser} from "../UserUtils"
 
 
