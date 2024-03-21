fbc40eeb16407b0a3d60488a0e0052190a4b7ef6
victoriaprisco <p.victoria@wustl.edu>
2024-03-20 22:53:18 -0500

    forgot one

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from "axios";
 import {getUser, getCurrentUser} from "../UserUtils"
-axios.defaults.withCredentials = true
 
 
 const Login = () => {
