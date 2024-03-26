5d4f9a6000ee1ecf4e05e918d4473c9d055337bf
victoriaprisco <p.victoria@wustl.edu>
2024-03-26 16:35:03 -0500

    add import

--- frontend/src/Components/ClassDetails.js
+++ frontend/src/Components/ClassDetails.js
@@ -10,7 +10,7 @@ import {
 import { getClassByID } from "../ClassUtils";
 import SimpleModal from "./SimpleModal";
 import ScheduleModal from "./ScheduleModal";
-import axios from "axios";
+import { getUserHoursForClass } from "../UserUtils";
 
 const ClassDetails = () => {
   const { classId } = useParams();
