38f715c19479e543e3665679a1a68ac82f273275
sammya24 <89807548+sammya24@users.noreply.github.com>
2024-04-18 13:59:33 -0500

    chat margin

--- frontend/src/Components/InputText.js
+++ frontend/src/Components/InputText.js
@@ -13,7 +13,7 @@ export default function InputText({ addMessage }) {
     return (
         <div className="flex justify-center items-center">
             <textarea
-                className="w-4/5 h-12 rounded-lg border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 shadow-sm focus:outline-none text-gray-700 py-2 px-4 block"
+                className="w-4/5 h-12 rounded-lg border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 shadow-sm focus:outline-none text-gray-700 py-2 px-4 bloc mt-2"
                 placeholder="Write something..."
                 value={message}
                 onChange={e => setMessage(e.target.value)}
